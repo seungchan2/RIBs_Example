87bd7ae347edf468d6ccb6dac6a39e89c8104fe6
chansenbbai <chanchu@hansung.ac.kr>
2022-07-04 10:28:36 +0900

    initial commit

---
+++ RIBs_Example/RIBs_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIBs_Example
+//
+//  Created by 김승찬 on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
